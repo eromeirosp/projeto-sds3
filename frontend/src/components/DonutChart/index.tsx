f9ffe9962231043748fc389fc0acf2a0fd89cfba
Eduardo Romeiro <eromeirosp@outlook.com>
2021-05-07 17:18:38 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,22 +11,35 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //Forma errada
-    let ChartData: ChartData = { labels: [], series: [] };
+    const [ChartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    //Forma Errada
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
+
+    //Forma errada
+    //let ChartData: ChartData = { labels: [], series: [] };
+
+    /*Forma Errada
+    //axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    //    .then(response => {
+    //        const data = response.data as SaleSum[];
+           const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            ChartData = {labels: myLabels, series: mySeries}
+            setChartData({ labels: myLabels, series: mySeries });
 
             console.log(ChartData);
 
         })
-
+        */
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
